27fbdde64d7aa65480dd07b1472b2ffb29a215fb
YouweiPeng <youwei@ualberta.ca>
2023-09-13 18:47:24 -0600

    new version of python scripts

--- lab1.py
+++ lab1.py
@@ -2,5 +2,5 @@ import requests
 
 #print (requests.__version__)
 
-response = response = requests.get('http://google.com/')
+response = response = requests.get('https://raw.githubusercontent.com/YouweiPeng/lab1/main/lab1.py')
 print(response.text)
